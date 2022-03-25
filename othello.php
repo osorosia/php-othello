9063a31cdcd4b2885e45ffc4c405a945f79cac77
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-25 19:41:45 +0900

    盤面出力を作成

--- othello.php
+++ othello.php
@@ -1,16 +1,66 @@
 <?php
 
 class Othello {
+    const WIDTH = 8;
+    const HEIGHT = 8;
+    const EMPTY = 0;
+    const BLACK = 1;
+    const WHITE = 2;
+    private $piece;
     private $board;
+    private $turn;
 
-    public function test() {
-        echo "hello\n";
+    // コンストラクタ
+    function __construct() {
+        $this->board = array(
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 2, 1, 0, 0, 0),
+            array(0, 0, 0, 1, 2, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+            array(0, 0, 0, 0, 0, 0, 0, 0),
+        );
+
+        $this->piece = array(
+            self::EMPTY => '.',
+            self::BLACK => 'o', 
+            self::WHITE => 'x',
+        );
+
+        $this->turn = self::BLACK;
+    }
+
+    // メイン関数
+    public function start() {
+        while (true) {
+
+            $this->print_board();
+            $this->print_turn();
+            break;
+        }
+    }
+
+    // 盤面を出力
+    private function print_board() {
+        foreach ($this->board as $row) {
+            foreach ($row as $cell) {
+                echo $this->piece[$cell];
+            }
+            echo "\n";
+        }
+    }
+
+    // ターンを出力
+    private function print_turn() {
+        echo "It's '" . $this->piece[$this->turn] . "' turn.\n";
     }
 }
 
 function main() {
-    $othello = new Othello();
-    $othello->test();
+    $game = new Othello();
+    $game->start();
 }
 
 main();
